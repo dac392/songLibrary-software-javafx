84e95c40b83c1e6d3e211321a776050640814c26
Abillou <82403488+Abillou@users.noreply.github.com>
2022-02-19 20:29:35 -0500

    Add files via upload

---
+++ songlibrary/controller/Song.java
@@ -0,0 +1,154 @@
+package songlibrary.controller;
+
+import javafx.collections.ObservableList;
+
+
+public class Song {
+	
+	private String title;
+	private String artist;
+	private String album;
+	private String year;
+	private int listIndex;
+	
+	
+	public Song(String title, String artist, String album, String year, int listIndex) {
+		
+		this.title = title;
+		this.artist = artist;
+		
+		this.album = "Unknown";
+		if(!album.equals("")) {
+			this.album = album;
+		}
+		
+		this.year = "Unknown";
+		if(!year.equals("")&&! year.toLowerCase().equals("unknown") )
+		{
+			int y = Integer.parseInt(year);
+		
+			this.year = (y > 0)? year : "unknown";
+		
+			this.setListIndex(listIndex);
+		}
+	}
+	
+
+	public Song(String[] info, int index) {
+		int d = -1;
+		if(!info[3].equals("") && intChecker(info[3]) == 0) {
+			d = Integer.parseInt(info[3]);
+		}
+		
+		this.listIndex = index;
+		this.title = info[0];
+		this.artist = info[1];
+		this.album = (!info[2].equals(""))? info[2]:"unknown";
+		this.year = (d > 0)? info[3] : "unknown";
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getArtist() {
+		return artist;
+	}	
+	
+	public String getAlbum() {
+		return album;
+	}	
+	
+	public String getYear() {
+		return year;
+	}
+	
+	public void setTitle(String a) {
+		if(a != null)
+		title = a;
+	}
+	
+	public void setArtist(String a) {
+		if(a != null)
+		artist = a;
+	}	
+	
+	public void setAlbum(String a) {
+		if(a != null)
+		album = a;
+	}	
+	
+	public void setYear(String a) {
+		if(Integer.parseInt(a)>0) {
+			year = a;
+		}
+		
+	}
+	public String toString() {
+		
+		String a = this.getTitle();
+		
+		//if(a.length() > 15)
+		//	a = a.substring(0,12) + "...";
+		
+		String b = this.getArtist();
+		//if(b.length() > 15)
+			//b = b.substring(0,12) + "...";
+		
+		String c = this.getAlbum();
+		//if(c.length() > 15)
+			//c = c.substring(0,12) + "...";
+		
+		String d = this.getYear();
+		//if(d.length() > 15)
+			//d = d.substring(0,12) + "...";
+		
+		
+		return ("Song: "+a+"\nArtist: "+b+"\nAlbum: "+c+"\nYear: "+d+"\n");
+	}
+
+	public int getListIndex() {
+		return listIndex;
+	}
+
+	public void setListIndex(int listIndex) {
+		this.listIndex = listIndex;
+	}
+
+
+	public boolean canBeAdded(ObservableList<String> obslist) {
+		if(obslist.size()>0) {
+			
+			String songTitle = "song: "+this.title.toLowerCase();
+			String songArtist = "artist: "+this.artist.toLowerCase();
+			
+			
+			for(int i = 0; i < obslist.size(); i++) {
+				String element = obslist.get(i);
+				String songInList[] = element.toLowerCase().split("\n");
+				
+				System.out.println(songInList[0] +" = "+songTitle);
+				System.out.println(songInList[1] +" = "+songArtist);
+				
+				if( songInList[0].equals(songTitle) && songInList[1].equals(songArtist)) {
+					return false;
+				}
+				
+			}
+		}
+
+		return true;
+	}
+	
+	private int intChecker(String potentialNum) {
+		int d = 0;
+		for(int i = 0; i < potentialNum.length(); i++) {
+			if(potentialNum.charAt(i)<'0'| potentialNum.charAt(i)>'9') {
+				d = -1;
+				break;
+			}
+		}
+		return d;
+		
+	}
+}
---
+++ songlibrary/main/SongLib.java
@@ -0,0 +1,41 @@
+package songlibrary.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import songlibrary.controller.SongLibController;
+
+public class SongLib extends Application{
+
+	public static void main(String[] args) {
+		
+		launch(args);
+	}
+	
+	public void start(Stage primaryStage){
+		try {		
+			FXMLLoader loader = new FXMLLoader();   
+			loader.setLocation(getClass().getResource("/songlibrary/views/SongLibrary.fxml"));
+			
+			// load the fxml
+			AnchorPane root = (AnchorPane)loader.load();
+
+			// get the controller (Do NOT create a new Controller()!!)
+			// instead, get it through the loader
+			SongLibController listController = loader.getController();
+			listController.start(primaryStage);
+		
+		
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
